a395e54f620950f80d84ca484b001a2adcf86ec4
Daniel O'Malley <omalled@lanl.gov>
2020-07-14 14:58:42 -0600

    add a test to make sure it matches ImplicitEuler() from DiffEq.jl

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 import DifferentiableBackwardEuler
 DBE = DifferentiableBackwardEuler
+using DifferentialEquations
+import LinearAlgebra
 import SparseArrays
 import Zygote
 
@@ -45,3 +47,15 @@ dgdp_zygote = Zygote.gradient((p, u0)->g(p, u0), p, u0)
 dgdp_naive = naivegradient((p, u0)->g(p, u0), p, u0)
 @test isapprox(dgdp_zygote[1], dgdp_naive[1]; atol=1e-4, rtol=1e-4)
 @test isapprox(dgdp_zygote[2], dgdp_naive[2]; atol=1e-4, rtol=1e-4)
+
+u0 = [1 / 2, 1 / 3]
+f(u, p, t) = f_u(u, p, t) * u
+f_u(u, p, t) = LinearAlgebra.Diagonal(fill(1.01, length(u0)))
+tspan = (0.0, 1.0)
+prob = ODEProblem(f, u0, tspan)
+sol = solve(prob, ImplicitEuler())
+dbesol = DBE.steps(u0, f, f_u, nothing, nothing, nothing, sol.t)
+for i = 1:length(sol.t)
+	@show sqrt(sum((dbesol[:, i] .- sol[:, i]) .^ 2))
+	@test isapprox(dbesol[:, i], sol[:, i])
+end
