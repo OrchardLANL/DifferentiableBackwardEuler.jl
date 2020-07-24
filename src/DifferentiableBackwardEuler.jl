ebdfd2846b00776b52c354a7ef9e5b9e1f914daa
Daniel O'Malley <omalled@lanl.gov>
2020-07-24 15:38:36 -0600

    switch from zygote to chainrules, add diffeq forward pass interface

--- src/DifferentiableBackwardEuler.jl
+++ src/DifferentiableBackwardEuler.jl
@@ -1,8 +1,9 @@
 module DifferentiableBackwardEuler
 
+import DifferentialEquations
+import ChainRulesCore
 import LinearAlgebra
 import NLsolve
-import Zygote
 
 #solve y1 - y0 - deltah * f(y1, p, t) = 0
 function step(y0, deltah, f, f_y, f_p, f_t, p, t; kwargs...)
@@ -19,33 +20,101 @@ function step(y0, deltah, f, f_y, f_p, f_t, p, t; kwargs...)
 	f!(F0, y0)
 	df = NLsolve.OnceDifferentiable(f!, j!, y0, F0, J0)
 	soln = NLsolve.nlsolve(df, y0; kwargs...)
+	if !NLsolve.converged(soln)
+		@show soln
+		@show t
+		error("solution did not converge")
+	end
 	return soln.zero
 end
 
-@Zygote.adjoint step(y0, deltah, f, f_y, f_p, f_t, p, t; kwargs...) = begin
-	y1 = step(y0, deltah, f, f_y, f_p, f_t, p, t)
-	back = delta->begin
+function make_step_pullback(y1, y0, deltah, f, f_y, f_p, f_t, p, t)
+	function step_pullback(delta)
 		step_y0 = -LinearAlgebra.I
 		step_deltah = -f(y1, p, t)
 		step_p = -deltah * f_p(y1, p, t)
 		step_t = -deltah * f_t(y1, p, t)
 		lambda = transpose(LinearAlgebra.I - deltah * f_y(y1, p, t)) \ delta
-		return (-transpose(step_y0) * lambda,#y0
-				-transpose(step_deltah) * lambda,#deltah
-				nothing, nothing, nothing, nothing,#f through f_t
-				-transpose(step_p) * lambda,#p
-				-transpose(step_t) * lambda)#t
+		return (ChainRulesCore.NO_FIELDS,#step function
+				@ChainRulesCore.thunk(-transpose(step_y0) * lambda),#y0
+				@ChainRulesCore.thunk(-transpose(step_deltah) * lambda),#deltah
+				ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS,#f through f_t
+				@ChainRulesCore.thunk(-transpose(step_p) * lambda),#p
+				@ChainRulesCore.thunk(-transpose(step_t) * lambda))#t
 	end
-	return y1, back
+	return step_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(step), y0, deltah, f, f_y, f_p, f_t, p, t; kwargs...)
+	y1 = step(y0, deltah, f, f_y, f_p, f_t, p, t; kwargs...)
+	pullback = make_step_pullback(y1, y0, deltah, f, f_y, f_p, f_t, p, t)
+	return y1, pullback
 end
 
 function steps(y0, f, f_y, f_p, f_t, p, ts; kwargs...)
-	ys = Zygote.Buffer(y0, length(y0), length(ts))
+	ys = zeros(eltype(y0), length(y0), length(ts))
 	ys[:, 1] = y0
 	for i = 1:length(ts) - 1
 		ys[:, i + 1] = step(ys[:, i], ts[i + 1] - ts[i], f, f_y, f_p, f_t, p, ts[i + 1]; kwargs...)
 	end
-	return copy(ys)
+	return ys
+end
+
+function make_steps_pullback(y, y0, f, f_y, f_p, f_t, p, ts)
+	step_pullbacks = [make_step_pullback(y[:, i], y[:, i - 1], ts[i] - ts[i - 1], f, f_y, f_p, f_t, p, ts[i]) for i = 2:size(y, 2)]
+	function steps_pullback(delta)
+		_, thisdy0, _, _, _, _, _, thisdp, _ = step_pullbacks[size(y, 2) - 1](delta[:, size(y, 2)])
+		dy0 = ChainRulesCore.unthunk(thisdy0) .+ delta[:, size(y, 2) - 1]
+		dp = ChainRulesCore.unthunk(thisdp)
+		for i = reverse(1:size(y, 2) - 2)
+			_, thisdy0, _, _, _, _, _, thisdp, _ = step_pullbacks[i](dy0)
+			dy0 .= ChainRulesCore.unthunk(thisdy0) .+ delta[:, i]
+			dp .= ChainRulesCore.unthunk(thisdp) .+ dp
+		end
+		return (ChainRulesCore.NO_FIELDS,#steps
+				dy0,#y0
+				ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS,#f through f_t
+				dp,#p
+				ChainRulesCore.Zero())#ts -- just make it zero for now
+	end
+	return steps_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(steps), y0, f, f_y, f_p, f_t, p, ts; kwargs...)
+	y = steps(y0, f, f_y, f_p, f_t, p, ts; kwargs...)
+	steps_pullback = make_steps_pullback(y, y0, f, f_y, f_p, f_t, p, ts)
+	return y, steps_pullback
+end
+
+function steps_diffeq(y0, f, f_y, f_p, f_t, p, t0, tfinal; kwargs...)
+	odef = DifferentialEquations.ODEFunction(f; jac=f_y, jac_prototype=f_y(y0, p, t0))
+	prob = DifferentialEquations.ODEProblem(odef, y0, (t0, tfinal), p)
+	soln = DifferentialEquations.solve(prob, DifferentialEquations.ImplicitEuler(); kwargs...)
+	return soln
+end
+function steps(y0, f, f_y, f_p, f_t, p, t0, tfinal; kwargs...)
+	return steps_diffeq(y0, f, f_y, f_p, f_t, p, t0, tfinal; kwargs...)[:, :]
+end
+
+function make_steps_pullback(y, y0, f, f_y, f_p, f_t, p, t0, tfinal, ts)
+	pullback1 = make_steps_pullback(y, y0, f, f_y, f_p, f_t, p, ts)
+	function steps_pullback(delta)
+		_, dy0, _, _, _, _, dp, _ = pullback1(delta)
+		return (ChainRulesCore.NO_FIELDS,#steps
+				dy0,#y0
+				ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS, ChainRulesCore.NO_FIELDS,#f through f_t
+				dp,#p
+				ChainRulesCore.Zero(),#t0 -- just make it zero for now
+				ChainRulesCore.Zero())#tfinal -- just make it zero for now
+	end
+	return steps_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(steps), y0, f, f_y, f_p, f_t, p, t0, tfinal; kwargs...)
+	soln = steps_diffeq(y0, f, f_y, f_p, f_t, p, t0, tfinal; kwargs...)
+	y = soln[:, :]
+	steps_pullback = make_steps_pullback(y, y0, f, f_y, f_p, f_t, p, t0, tfinal, soln.t)
+	return y, steps_pullback
 end
 
 end
--- tests/runtests.jl
+++ tests/runtests.jl
@@ -11,6 +11,7 @@ f_u(u, p, t) = SparseArrays.spdiagm(0=>p)
 f_p(u, p, t) = SparseArrays.spdiagm(0=>u)
 f_t(u, p, t) = zero(u)
 
+#test the forward solver analytically
 n = 2
 p = collect(1.0:n)
 u0 = ones(n)
@@ -22,11 +23,7 @@ for i = 1:length(ts) - 1
 end
 @test isapprox(exp.(p), us[end]; atol=1e2 / numsteps, atol=1e2 / numsteps)
 
-numsteps = 10 ^ 3
-ts = collect(range(0, 1e0; length=numsteps))
-g(p, u0) = sum(DBE.steps(u0, f, f_u, f_p, f_t, p, ts)[:, end])
-@test isapprox(sum(exp.(p * ts[end])), g(p, u0); atol=1e2 / numsteps, rtol=1e2 / numsteps)
-
+#test gradients involving step(...)
 function naivegradient(f, x0s...; delta::Float64=1e-8)
 	f0 = f(x0s...)
 	gradient = map(x->zeros(size(x)), x0s)
@@ -42,19 +39,47 @@ function naivegradient(f, x0s...; delta::Float64=1e-8)
 	end
 	return gradient
 end
+g(p, u0) = sum(DBE.step(u0, ts[2] - ts[1], f, f_u, f_p, f_t, p, ts[2]))
+dgdp_zygote = Zygote.gradient((p, u0)->g(p, u0), p, u0)
+dgdp_naive = naivegradient((p, u0)->g(p, u0), p, u0)
+@test isapprox(dgdp_zygote[1], dgdp_naive[1]; atol=1e-6, rtol=1e-6)
+@test isapprox(dgdp_zygote[2], dgdp_naive[2]; atol=1e-6, rtol=1e-6)
 
+#test gradients involving steps(..., ts)
+numsteps = 10 ^ 1
+ts = collect(range(0, 1e0; length=numsteps))
+g(p, u0) = sum(DBE.steps(u0, f, f_u, f_p, f_t, p, ts)[:, end])
+@test isapprox(sum(exp.(p * ts[end])), g(p, u0); atol=1e2 / numsteps, rtol=1e2 / numsteps)
 dgdp_zygote = Zygote.gradient((p, u0)->g(p, u0), p, u0)
 dgdp_naive = naivegradient((p, u0)->g(p, u0), p, u0)
 @test isapprox(dgdp_zygote[1], dgdp_naive[1]; atol=1e-4, rtol=1e-4)
 @test isapprox(dgdp_zygote[2], dgdp_naive[2]; atol=1e-4, rtol=1e-4)
 
+#test the forward solver against DifferentialEquations.jl
 u0 = [1 / 2, 1 / 3]
 f(u, p, t) = f_u(u, p, t) * u
-f_u(u, p, t) = LinearAlgebra.Diagonal(fill(1.01, length(u0)))
+f_u(u, p, t) = LinearAlgebra.Diagonal(copy(p))
 tspan = (0.0, 1.0)
-prob = ODEProblem(f, u0, tspan)
+prob = ODEProblem(f, u0, tspan, p)
 sol = solve(prob, ImplicitEuler())
-dbesol = DBE.steps(u0, f, f_u, nothing, nothing, nothing, sol.t)
+dbesol = DBE.steps(u0, f, f_u, nothing, nothing, p, sol.t)
 for i = 1:length(sol.t)
 	@test isapprox(dbesol[:, i], sol[:, i])
 end
+
+#test the forward runs involving steps(..., t0, final) are working
+dbesol = DBE.steps(u0, f, f_u, nothing, nothing, p, tspan[1], tspan[2])
+for i = 1:length(sol.t)
+	@test isapprox(dbesol[:, i], sol[:, i])
+end
+
+#test gradients involving steps(..., t0, tfinal)
+t0 = 0.0
+tfinal = 1e0
+g(p, u0) = sum(DBE.steps(u0, f, f_u, f_p, f_t, p, t0, tfinal)[:, end])
+g_val = g(p, u0)
+@test isapprox(sum(u0 .* exp.(p * ts[end])), g_val; atol=1e2 / numsteps, rtol=1e2 / numsteps)
+dgdp_zygote = Zygote.gradient(g, p, u0)
+dgdp_naive = naivegradient(g, p, u0; delta=1e-2)
+@test isapprox(dgdp_zygote[2], dgdp_naive[2]; atol=1e-4, rtol=1e-4)
+@test isapprox(dgdp_zygote[1], dgdp_naive[1]; atol=1e-2, rtol=1e-2)
